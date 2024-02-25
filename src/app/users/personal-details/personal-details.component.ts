e6fd22a4724a6e987b4a5d02ee4cfdf5391fe771
flcristian <qflorescucristian@gmail.com>
2024-02-25 19:45:38 +0200

    Dropdown-ul de gender nu ia valoarea initiala specificata

--- src/app/users/personal-details/personal-details.component.ts
+++ src/app/users/personal-details/personal-details.component.ts
@@ -1,88 +1,106 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {Subscription} from "rxjs";
-import {User} from "../models/user.model";
-import {DoctorUiStateService} from "../services/doctor-ui-state.service";
-import {Router} from "@angular/router";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {Confirmation, ConfirmationService} from "primeng/api";
-import {UpdateUserRequest} from "../models/update-user-request.model";
-import {ConfirmPopup} from "primeng/confirmpopup";
-import {findLastMappingIndexBefore} from "@angular/compiler-cli/src/ngtsc/sourcemaps/src/source_file";
+import { ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Subscription } from "rxjs";
+import { User } from "../models/user.model";
+import { DoctorUiStateService } from "../services/doctor-ui-state.service";
+import { Router } from "@angular/router";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Confirmation, ConfirmationService } from "primeng/api";
+import { UpdateUserRequest } from "../models/update-user-request.model";
+import { ConfirmPopup } from "primeng/confirmpopup";
 
 @Component({
   selector: 'app-personal-details',
   templateUrl: './personal-details.component.html'
 })
-export class PersonalDetailsComponent implements OnInit {
+export class PersonalDetailsComponent implements OnInit, OnDestroy {
   private subscriptions = new Subscription();
-  protected editBase: boolean = false
-  protected editContact: boolean = false
+  protected editBase: boolean = false;
+  protected editContact: boolean = false;
   protected doctor: User | null = null;
   protected genders = [
-    {
-      label: "Male",
-      value: "Male"
-    },
-    {
-      label: "Female",
-      value: "Female"
-    }
-  ]
+    { label: "Male", value: "Male" },
+    { label: "Female", value: "Female" }
+  ];
   private error: String | null = null;
-  baseForm: FormGroup = new FormGroup({})
-
-  contactForm: FormGroup = new FormGroup({})
-
+  baseForm: FormGroup = new FormGroup({});
+  contactForm: FormGroup = new FormGroup({});
   @ViewChild(ConfirmPopup) confirmPopup!: ConfirmPopup;
 
   constructor(
     private stateService: DoctorUiStateService,
-    private confirmationService : ConfirmationService,
-    private router: Router
-  ) { }
+    private confirmationService: ConfirmationService,
+    private router: Router,
+  ) {
+
+
+
+  }
 
   ngOnInit() {
-    this.subscriptions.add(this.getDoctor())
+    this.initializeForms();
+    this.subscriptions.add(this.getDoctor());
+
+    
+  }
+
+  initializeForms() {
+    this.baseForm = new FormGroup({
+      name: new FormControl('', [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(128)
+      ]),
+      age: new FormControl('', [
+        Validators.required,
+        Validators.min(0)
+      ]),
+      gender: new FormControl('Female')
+    },{updateOn:'change'});
+
+    this.contactForm = new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email
+      ]),
+      phone: new FormControl('', [
+        Validators.required
+      ])
+    },{updateOn:'change'});
   }
 
   getDoctor() {
     return this.stateService.state$.subscribe({
       next: (data) => {
-        this.doctor = data.doctor
-        this.error = data.error
-
-        this.baseForm = new FormGroup({
-          name: new FormControl(this.doctor?.name, [
-            Validators.required,
-            Validators.minLength(1),
-            Validators.maxLength(128)
-          ]),
-          age: new FormControl(this.doctor?.age, [
-            Validators.required,
-            Validators.min(0)
-          ]),
-          gender: new FormControl('Female')
-        },)
-
-        this.contactForm = new FormGroup({
-          email: new FormControl(this.doctor?.email, [
-            Validators.required,
-            Validators.email
-          ]),
-          phone: new FormControl(this.doctor?.phone, [
-            Validators.required
-          ])
-        })
+        this.doctor = data.doctor;
+        this.error = data.error;
+        if (this.doctor) {
+          this.populateForms(this.doctor);
+        }
       },
       error: (error) => {
-        this.stateService.setError(error)
+        this.stateService.setError(error);
       }
-    })
+    });
   }
 
-  // Form Confirmations
+  populateForms(doctor: User) {
+    this.baseForm.patchValue({
+      name: doctor.name,
+      age: doctor.age,
+      gender: doctor.gender
+    });
 
-  updateDoctor(event: Event){
+    this.contactForm.patchValue({
+      email: doctor.email,
+      phone: doctor.phone
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
+
+  updateDoctor(event: Event) {
     const confirmation: Confirmation = {
       target: event.target as EventTarget,
       message: 'Are you sure you want to update your details?',
@@ -96,20 +114,20 @@ export class PersonalDetailsComponent implements OnInit {
           phone: this.contactForm.value.phone as string,
           password: this.doctor?.password as string,
           type: this.doctor?.type as number
-        }
+        };
 
-        this.stateService.updateUser(request)
+        this.stateService.updateUser(request);
       }
     };
 
     this.confirmationService.confirm(confirmation);
   }
 
-  acceptUpdate(){
-    this.confirmPopup.accept()
+  acceptUpdate() {
+    this.confirmPopup.accept();
   }
 
   rejectUpdate() {
-    this.confirmPopup.reject()
+    this.confirmPopup.reject();
   }
 }
